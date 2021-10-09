da0b1b82f909dc9a1a896b6ac93cbf608ef0f815
Feraszo <92191526+Feraszo@users.noreply.github.com>
2021-10-09 08:32:33 +0300

    Add files via upload

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCvStruHblZsoJpEG28mLJGKOiUsSxw88g",
-    authDomain: "medical-simulation-q.firebaseapp.com",
-    databaseURL: "https://medical-simulation-q-default-rtdb.firebaseio.com",
-    projectId: "medical-simulation-q",
-    storageBucket: "medical-simulation-q.appspot.com",
-    messagingSenderId: "977919033741",
-    appId: "1:977919033741:web:5be98753102df191853dc4",
-    measurementId: "G-156X0R6G80"
+  apiKey: "AIzaSyCvStruHblZsoJpEG28mLJGKOiUsSxw88g",
+  authDomain: "medical-simulation-q.firebaseapp.com",
+  databaseURL: "https://medical-simulation-q-default-rtdb.firebaseio.com",
+  projectId: "medical-simulation-q",
+  storageBucket: "medical-simulation-q.appspot.com",
+  messagingSenderId: "977919033741",
+  appId: "1:977919033741:web:5be98753102df191853dc4",
+  measurementId: "G-156X0R6G80"
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
